1b25f38f60e275a71e8bd463d720fcf648b033fc
Daniel Adams <msub2official@gmail.com>
2022-12-06 03:13:32 -0500

    Bundle library into a single file!
    FINALLY

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import webWorkerLoader from 'rollup-plugin-web-worker-loader';
+import terser from '@rollup/plugin-terser';
+
+const formats = ['esm', 'esm-min']
+
+export default formats.map(format => {
+  const basePath = 'dist/sepia-speechrecognition-polyfill';
+  const terserPlugin = format === 'esm' ? () => {} : terser;
+  return {
+    input: 'src/index.js',
+    output: {
+      file: `${basePath}.${format === 'esm' ? 'js' : 'min.js'}`,
+      format: 'esm'
+    },
+    plugins: [
+      terserPlugin(),
+      webWorkerLoader({ targetPlatform: 'browser' })
+    ]
+  }
+});
